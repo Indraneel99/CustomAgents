8275057b47ff4b8ff713344d03c39a177a9e2136
indra <indraneelkumar3@gmail.com>
2024-04-04 18:01:24 +0530

    Added streamlit frontend

--- client.py
+++ client.py
@@ -1,6 +1,14 @@
 import requests
+import streamlit as st
 
-response = requests.post("http://localhost:8000/invoke",
-                         json = {'input' : {'input':'what is the sum of 2 and 3'}})
+def get_response(input):
 
-print(response.json()['output']['output'])
+    response = requests.post("http://localhost:8000/invoke",
+                             json = {'input' : {'input':input}})
+
+    return response.json()['output']['output']
+
+st.title("Ask the Agent")
+input_text = st.text_input("Ask any math questions or news around the world")
+if input_text:
+    st.write(get_response(input_text))
